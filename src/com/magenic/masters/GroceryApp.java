ec0f2bfdfcb4762c85c4b9857c14b9539c862fdf
Runette Orobia <runette.orobia@calabrio.com>
2021-12-02 02:51:35 +0800

    display items

--- src/com/magenic/masters/GroceryApp.java
+++ src/com/magenic/masters/GroceryApp.java
@@ -7,54 +7,111 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Predicate;
 
 public class GroceryApp {
 
+    private static final String MAIN_MENU = """
+                Category:
+                    1 - Pantry supplies
+                    2 - Meat/Poultry/Seafood
+                    3 - Snacks
+                    
+                Choose Category(-1 to Checkout, -2 to Exit):
+                """;
+
+    private static final String ITEM_FORMAT = "[%d]%s %10s/%s";
+
+    private Scanner scanner;
+    private List<Item> itemsInCart;
+    private List<Item> items;
+
+    public GroceryApp(List<Item> items) {
+        this.items = items;
+        this.scanner = new Scanner(System.in);
+        this.itemsInCart = new ArrayList<>();
+    }
+
+    private int getValidIntInput(String message) {
+        int userInput = 0;
+        boolean isValid = false;
+        do {
+            try {
+                System.out.print(message);
+                userInput = input.nextInt();
+                isValid = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a valid number.\n");
+            }
+            input.nextLine();
+        } while (!isValid);
+
+        return userInput;
+    }
+
+    private void displayMainMenu() {
+        int choice = getValidIntInput(MAIN_MENU)
+        switch (choice) {
+            case 1 -> {
+                System.out.println("Pantry Items:");
+                displayItems(Category.PANTRY);
+            }
+            case 2 -> {
+                System.out.println("Meat/Poultry/Seafood Items:");
+                displayItems(Category.MPS);
+            }
+            case 3 -> {
+                System.out.println("Snack Items:");
+                displayItems(Category.SNACKS);
+            }
+            case -1 -> System.out.println("checkout");
+            case -2 -> {
+                System.out.println("Unsupported option: Program Exiting now...");
+            }
+            default -> {
+                System.out.println("Invalid input. Try again.");
+                displayMainMenu();
+            }
+        }
+    }
+
+    private void displayItems(Category category) {
+        AtomicInteger id = new AtomicInteger();
+        items.stream()
+                .filter(item -> item.getCategory() == category)
+                .forEach(item -> {
+                    System.out.println(ITEM_FORMAT.formatted(id.getAndIncrement(), item.getName(), item.getPrice(), item.getUnit()));
+                });
+    }
+
     public static void main(String[] args) {
+
+        NumberFormat formatter = NumberFormat.getCompactNumberInstance(Locale.US, NumberFormat.Style.SHORT);
+        formatter.setMinimumFractionDigits(2);
 
         List<Item> items = new ArrayList<>();
         try {
             Path filepath = Paths.get("C:/Users/RunetteO/workspace/stocks.csv");
             String content = Files.readString(filepath);
 
-            content.lines().filter(Predicate.not(String::isBlank)).forEach((var line) ->{
+            content.lines().filter(Predicate.not(String::isBlank)).forEach((var line) -> {
                 String[] details = line.split(",");
                 Item item = new Item(details[0], Double.valueOf(details[1]), details[2], Category.getByDescription(details[3])); //validation?
                 items.add(item);
             });
 
+            GroceryApp app = new GroceryApp(items);
+            app.displayMainMenu();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        String menu1 = """
-                Category:
-                    1 - Pantry supplies
-                    2 - Meat/Poultry/Seafood
-                    3 - Snacks
-                    
-                Choose Cateogry(-1 to Checkout, -2 to Exit):
-                """;
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.print(menu1);
-
-        int choice = scanner.nextInt();
-        switch (choice) {
-            case 1 -> System.out.printf("gdsfgsd");
-            case 2 -> System.out.printf("gdsfgsd");
-            case 3 -> System.out.printf("gdsfgsd");
-            default -> System.out.println("exit");
-        }
-
-        System.out.println("ITEMS:");
-        items.forEach(System.out::println);
-
-        //tuloy ko mamaya after shift hehe
 
     }
 
