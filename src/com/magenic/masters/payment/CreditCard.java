213d10ad2db283e789da024adf7bb2c90a571f15
Pauline Eliseo <PaulineE@magenic.com>
2021-12-15 19:07:10 +0800

    updated credit card final class

--- src/com/magenic/masters/payment/CreditCard.java
+++ src/com/magenic/masters/payment/CreditCard.java
@@ -2,17 +2,20 @@ package com.magenic.masters.payment;
 
 public final class CreditCard implements PaymentMethod {
 
+	 private final String  accountName = "Mau Tuazon";
+     private final String  accountNumber = "4028123456789012";
+
     @Override
     public String getFileName() {
         return "receipt_cc.txt";
     }
 
     @Override
-	public String getAccountDetails() {
-        return """
-            Name on card: David Beckham
-            Credit card number: 4028123456789012
-            Expiry date: 12/2022
-            """;
+    public String getAccountDetails() {
+        String details = """
+				Name on Card:\s%s
+				Credit Card number:\s%s
+				Expiry Date:\s12/2022""";
+        return details.formatted(this.accountName, this.accountNumber);
     }
 }
